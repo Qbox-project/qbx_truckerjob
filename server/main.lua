34b2b0ba53d07b1666283159e8e4a2c3d9decb2c
ArturMichalak <arturmichalak96@gmail.com>
2024-06-15 19:50:16 +0200

    feat: convert to ox zones

--- server/main.lua
+++ server/main.lua
@@ -182,7 +182,7 @@ lib.callback.register('qbx_truckerjob:server:getNewTask', function(source, init)
 
     local doneLocations = locations[source].done
     locations[source].done[#doneLocations + 1] = locations[source].current
-    if #doneLocations == config.maxDrops then
+    if #doneLocations == config.maxLocations then
         locations[source].current = nil
         return 0, 0
     end
